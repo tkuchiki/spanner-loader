437c77aa17c919d79bc12bb0b5beaf239bdbf4ed
tkuchiki <kuchiki.taku@gmail.com>
2023-10-25 11:55:35 +0900

    Support CPU Priority

---
+++ Makefile
@@ -0,0 +1,6 @@
+GO_LDFLAGS=-s -w
+GIT_COMMIT := $(shell git rev-parse --short HEAD)
+
+.PHONY: build
+build:
+	go build -trimpath -ldflags "$(GO_LDFLAGS) -X=main.version=$(GIT_COMMIT)"
--- main.go
+++ main.go
@@ -5,9 +5,12 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 	"time"
 
 	"cloud.google.com/go/spanner"
+	sppb "cloud.google.com/go/spanner/apiv1/spannerpb"
 	"github.com/kelseyhightower/envconfig"
 	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc/codes"
@@ -18,6 +21,7 @@ type Config struct {
 	GCPProjectID    string `envconfig:"GCP_PROJECT_ID"`
 	SpannerInstance string `envconfig:"SPANNER_INSTANCE"`
 	SpannerDatabase string `envconfig:"SPANNER_DATABASE"`
+	CPUPriority     string `envconfig:"CPU_PRIORITY"`
 }
 
 func dbpath(projectID, instanceName, databaseName string) string {
@@ -28,6 +32,31 @@ func newSpannerClient(ctx context.Context, projectID, instanceName, databaseName
 	return spanner.NewClientWithConfig(ctx, dbpath(projectID, instanceName, databaseName), config)
 }
 
+func checkPriority(priority string) error {
+	p := strings.ToLower(priority)
+
+	if p == "high" || p == "medium" || p == "low" {
+		return nil
+	}
+
+	return fmt.Errorf("invalid priority: %s,　You can set the CPU priority as low, medium, or high", priority)
+}
+
+func priorityStringToRequestOption(priority string) sppb.RequestOptions_Priority {
+	switch strings.ToLower(priority) {
+	case "low":
+		return sppb.RequestOptions_PRIORITY_LOW
+	case "medium":
+		return sppb.RequestOptions_PRIORITY_MEDIUM
+	case "high":
+		return sppb.RequestOptions_PRIORITY_HIGH
+	default:
+		return sppb.RequestOptions_PRIORITY_UNSPECIFIED
+	}
+}
+
+var version string
+
 func main() {
 	var con uint64
 	var duration time.Duration
@@ -35,13 +64,22 @@ func main() {
 	var project string
 	var instance string
 	var db string
+	var priorityStr string
+	var showVersion bool
 	flag.Uint64Var(&con, "c", 1, "Concurrency")
 	flag.DurationVar(&duration, "d", 60*time.Second, "Duration")
 	flag.StringVar(&query, "query", "", "SQL")
 	flag.StringVar(&project, "project", "", "GCP Project")
 	flag.StringVar(&instance, "instance", "", "Cloud Spanner Instance")
 	flag.StringVar(&db, "database", "", "Cloud Spanner Database")
+	flag.StringVar(&priorityStr, "priority", "high", "CPU priority: low, medium, high")
+	flag.BoolVar(&showVersion, "version", false, "Show version")
 	flag.Parse()
+
+	if showVersion {
+		fmt.Println(version)
+		os.Exit(0)
+	}
 
 	if query == "" {
 		log.Fatal("-query is required")
@@ -50,6 +88,10 @@ func main() {
 	var c Config
 	err := envconfig.Process("spanner-loader", &c)
 	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := checkPriority(priorityStr); err != nil {
 		log.Fatal(err)
 	}
 
@@ -64,6 +106,12 @@ func main() {
 	if db != "" {
 		c.SpannerDatabase = db
 	}
+
+	if priorityStr != "" {
+		c.CPUPriority = priorityStr
+	}
+
+	priority := priorityStringToRequestOption(c.CPUPriority)
 
 	ctx, cancel := context.WithTimeout(context.Background(), duration)
 	defer cancel()
@@ -88,7 +136,7 @@ func main() {
 	for i := uint64(0); i < con; i++ {
 		eg.Go(func() error {
 			for {
-				iter := client.Single().Query(ctx, spanner.Statement{SQL: query})
+				iter := client.Single().QueryWithOptions(ctx, spanner.Statement{SQL: query}, spanner.QueryOptions{Priority: priority})
 				for {
 					_, err := iter.Next()
 
